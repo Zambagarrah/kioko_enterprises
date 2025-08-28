8a1eb03ae7ede54e50a32db41ebce4d16ccdf790
Zambagarrah <zambagarrah@users.noreply.github.com>
2025-08-28 17:17:49 +0300

    fix: imported paypal_callback in core/urls.py & fixed syntax issues

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from .views import (
     mpesa_callback,
     airtel_callback,
+    paypal_callback,
 )
 
 urlpatterns = [
     path('mpesa/callback/', mpesa_callback, name='mpesa_callback'),
-    path('airtel/callback/', airtel_callback, name='airtel_callback')
-    path('paypal/callback/', views.paypal_callback, name='paypal_callback'),
+    path('airtel/callback/', airtel_callback, name='airtel_callback'),
+    path('paypal/callback/', paypal_callback, name='paypal_callback'),
 ]
