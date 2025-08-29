24c79c6c7849e13eee091340ca0b2e80ac59cb49
Zambagarrah <zambagarrah@users.noreply.github.com>
2025-08-29 13:26:27 +0300

    fix: cleaned up the code

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
-
+import core.signals
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+def ready(self):
